c1882a7d4c5647d9a84e5a0adc478ac383ebe2f0
Shaun Einolf <seinolf@gmail.com>
2014-12-30 11:37:21 -0500

    Remove file extension error when adding an xml file after trying to upload a non-xml file

--- js/load_xml.js
+++ js/load_xml.js
@@ -78,13 +78,14 @@ function bind_uploader() {
 			FilesAdded: function(up, files) {
 				plupload.each(files, function(file) {
 					document.getElementById('filelist').innerHTML = '<div id="' + file.id + '">' + file.name + ' (' + plupload.formatSize(file.size) + ') <b></b></div>';
+						document.getElementById('console').innerHTML = "";
 				});
 			},
 			UploadProgress: function(up, file) {
 				document.getElementById(file.id).getElementsByTagName('b')[0].innerHTML = '<span>' + file.percent + "%</span>";
 			},
 			Error: function(up, err) {
-				document.getElementById('console').innerHTML += "\nError #" + err.code + ": " + err.message;
+				document.getElementById('console').innerHTML = "\nError #" + err.code + ": " + err.message;
 			},
 			FileUploaded: function(upldr, file, obj) {
 				if(file.getNative() !== null) {
