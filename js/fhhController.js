3f0d17d0b3c83cc6766ff5b056538da863e65b2d
Shaun Einolf <seinolf@gmail.com>
2015-05-10 01:09:40 -0400

    very good progress on health table. 90% complete

--- js/fhhController.js
+++ js/fhhController.js
@@ -30,9 +30,9 @@ app.controller('fhhController', ['$rootScope','$scope','$window','$timeout','$mo
 
 }]);
 
-app.controller('tableController', ['$scope','$modalInstance', function ($scope,$modalInstance) {
+app.controller('tableController', ['$scope','$modalInstance','$timeout', function ($scope,$modalInstance,$timeout) {
+	var initialDiseases = ['SNOMED_CT-56265001','SNOMED_CT-116288000','SNOMED_CT-73211009','SNOMED_CT-363406005','SNOMED_CT-254837009','SNOMED_CT-363443007']; 	
 	var STATICDISEASES = ['SNOMED_CT-56265001','SNOMED_CT-116288000','SNOMED_CT-73211009','SNOMED_CT-363406005','SNOMED_CT-254837009','SNOMED_CT-363443007']; 	
-
 	// funtion to translate items //
 	$scope.translate = function(prefix,t_string) {
 		if (prefix=="diseases") {
@@ -43,6 +43,29 @@ app.controller('tableController', ['$scope','$modalInstance', function ($scope,$
 		}
 	}	
 
+	$scope.collapse_status = {'isCollapsed':false,'collapse_text':$scope.translate("fhh_family_pedigree","collapse_table")};
+	
+	// get language for legend key //
+	$scope.lng = window.i18n.lng();
+	if ($scope.lng=='en-US') {
+	   $scope.lng = 'en';
+	};  	
+
+	// collapse dynamic diseases into one column //
+	$scope.collapseTable = function() {
+	if ($scope.collapse_status.isCollapsed) {
+		$scope.collapse_status.isCollapsed = false;
+		$scope.collapse_status.collapse_text = $scope.translate("fhh_family_pedigree","collapse_table");
+	}
+	else {
+		$scope.collapse_status.isCollapsed = true;
+		$scope.collapse_status.collapse_text = $scope.translate("fhh_family_pedigree","expand_table");
+	}
+	$scope.disease_list=getDiseases();
+	};
+
+	
+
 	// calculates age based on date of birth //
 	$scope.calculateAge = function calculateAge(birthday) { // birthday is a date
 		var birthday = new Date(birthday);
@@ -51,6 +74,7 @@ app.controller('tableController', ['$scope','$modalInstance', function ($scope,$
 		return Math.abs(ageDate.getUTCFullYear() - 1970);
 	};	
 
+	// this needs fixed //
 	$scope.calculate_bmi = function(w,h) {
 	    var finalBmi=0;
 	    var weight = w;
@@ -63,23 +87,35 @@ app.controller('tableController', ['$scope','$modalInstance', function ($scope,$
 	    return finalBmi.toFixed(2);
 	}
 
-	$scope.calculcate_report_date = function() {
-
-		var lng = window.i18n.lng();
-		if (lng=='en-US') {
-		   lng = 'en';
-		};  		
+	// calculates report date in the correct language //
+	$scope.calculcate_report_date = function() { 		
 		var options = {weekday: "long", year: "numeric", month: "long", day: "numeric", hour:"numeric",minute:"numeric",hour12:"true"};             
-		var today = $.t("fhh_family_pedigree.date_of_report") + ": " + new Date().toLocaleString(lng, options);		
+		var today = $.t("fhh_family_pedigree.date_of_report") + ": " + new Date().toLocaleString($scope.lng, options);		
 		return today
 	};
 
 	// creates javascript object of static diseases, including their translated names // 
 	var getDiseases = function() {
-		var dl = []
+		var dl = [];
+		var otherDiseases = false;
+		console.log($scope.collapse_status)
 		for (key in STATICDISEASES) {
-			d = STATICDISEASES[key];
-			dl.push({'code':d,'translatedDiseaseName':$scope.translate("diseases",d)})
+			if ($scope.collapse_status.isCollapsed) { // table has been collapsed
+				if (initialDiseases.indexOf(STATICDISEASES[key])>-1) { // create normal disease column
+					d = STATICDISEASES[key];
+					dl.push({'code':d,'translatedDiseaseName':$scope.translate("diseases",d),'show':true})
+				}	
+				else {
+					if (!otherDiseases) {
+						dl.push({'code':'1','translatedDiseaseName':$scope.translate("fhh_family_pedigree","other_disease"),'show':true});
+						otherDiseases = true;
+					}
+				}			
+			}
+			else {
+				d = STATICDISEASES[key];
+				dl.push({'code':d,'translatedDiseaseName':$scope.translate("diseases",d),'show':true})
+			}
 		}
 		return dl;
 	};
@@ -88,31 +124,147 @@ app.controller('tableController', ['$scope','$modalInstance', function ($scope,$
 	// create list containing personal information records and retrieving other diseases not in static list //
 	$scope.personal_information = function() {
 		var new_personal_information = [];
+		var pi = personal_information;
+		var self_health_history = $scope.createHealthHistory([],[],pi['Health History'])
+		new_personal_information.push({'name':pi.name,'gender':pi.gender,'relationship':$scope.translate("fhh_js","self"),'is_alive':$scope.translate("fhh_family_pedigree","alive"),'Health History':self_health_history.health_history,'health_code_lookup':self_health_history.health_code_lookup})
 		for (x in personal_information) {
 			var o = personal_information[x];
 			if (o!=undefined) {
 				if (o.gender!=undefined)
-				{
-					for (x in o['Health History']) {
-					  console.log(o['Health History'][x])
-					  if (STATICDISEASES.indexOf(o['Health History'][x]['Disease Code'])==-1) {
-						  STATICDISEASES.push(o['Health History'][x]['Disease Code'])
-					  }
-					} 	
-					new_personal_information.push(o);
-				}
+					{
+						var person_dict = {};
+						var health_history = [];
+						var health_code_lookup = [];
+						person_dict["is_alive"]=$scope.translate("fhh_family_pedigree",o.is_alive);
+						person_dict["name"]=o.name;
+						person_dict["relationship"]=$scope.translate("fhh_family_pedigree",o.relationship);
+						if (o.cause_of_death_code) {
+							person_dict['cause_of_death']=$scope.translate("diseases",o.cause_of_death_code)						
+							person_dict['estimated_death_age']=$scope.translate("fhh_js",o.estimated_death_age)						
+						}
+						var hh = $scope.createHealthHistory(health_history,health_code_lookup,o['Health History'])
+						person_dict['Health History']=hh.health_history;
+						person_dict['health_code_lookup']=hh.health_code_lookup;
+						new_personal_information.push(person_dict)
+					}
 			}
 		}
 		return new_personal_information;
 	};
 
+	// creates health history for individuals. Also adds diseases to STATIC list that dont already exist //
+	$scope.createHealthHistory = function(health_history,health_code_lookup,history) {
+		for (x in history) {
+			var health_history_entry = {};
+			health_history_entry["Disease Code"]=history[x]['Disease Code'];
+			health_code_lookup.push(history[x]['Disease Code']);
+			health_history_entry["Age At Diagnosis"]=$scope.translate("fhh_js",history[x]['Age At Diagnosis']);
+			health_history_entry["translatedDiseaseName"]=$scope.translate("diseases",history[x]['Disease Code']);
+			if (STATICDISEASES.indexOf(history[x]['Disease Code'])==-1) {
+			  STATICDISEASES.push(history[x]['Disease Code'])
+			  health_history_entry["isOther"]=true;
+			}
+			health_history.push(health_history_entry)
+		} 		
+		return {'health_history':health_history,'health_code_lookup':health_code_lookup}
+	};
+
+	// looks up if health record(hr) has particular disease code(dc) for table column //
+	$scope.lookupDisease = function(hr, dc,diagram) {
+		disease_is_true=hr.health_code_lookup.indexOf(dc)
+		if (disease_is_true>-1) {
+			if (diagram) {
+				return hr['Health History'][disease_is_true]['Age At Diagnosis']
+			}
+			else {
+				return hr['Health History'][disease_is_true]
+			}
+		}
+		return "";	
+	};
+
+	// define personal information, self and disease list variables //
 	$scope.pi = $scope.personal_information(); // set pi to personal information function //
 	$scope.self_history = personal_information;
 	$scope.disease_list = getDiseases();
-	// $scope.translatedVariables = {'title':$scope.translate("fhh_family_pedigree","title"),'zoom_in': $scope.translate("fhh_family_pedigree","zoom_in"),'zoom_out':$scope.translate("fhh_family_pedigree","zoom_out"),'close':$scope.translate("fhh_family_pedigree","close"),'print':$scope.translate("fhh_family_pedigree","print"),'desc_1':$scope.translate("fhh_family_pedigree","desc_line1"),'desc_2':$scope.translate("fhh_family_pedigree","desc_line2"),'diagram_table_options':$scope.translate("fhh_family_pedigree","diagram_options"),'save_image':$scope.translate("fhh_family_pedigree","save_image"),'my_personal_information':}
-	$scope.ok = function () {
+
+	// hide disease in table when clicking the X icon //
+	$scope.removeDisease = function(d) {
+		d.show=false;
+	};
+
+	// show disease in table when clicking the + icon //
+	$scope.showDisease = function(d) {
+		d.show=true;
+	};	
+
+	// print table using gojs and converting to an image //
+	$scope.print = function(print) {
+		$scope.diagram_show=true;
+		if (!$scope.diagram) 
+		{
+			$timeout(function() {
+				$scope.displayHiddenDiagram();
+			});  				
+		};
+			$timeout(function() {
+				var i = $scope.diagram.makeImageData();
+					$scope.diagram_show=false;
+
+					var WindowObject = window.open("", "Table",
+	    				"width="  + $("table.health_table").width() + ",height=" + $("table.health_table").height() + ",top=50,left=50,toolbars=no,scrollbars=yes,status=no,resizable=yes");
+	    				WindowObject.document.writeln('<html><head></head><body><div style="padding-left:10px;font-weight:bold;">' + $("div#personal_info").html() + '</div><img src="' + i + '"></body></html>');
+				  	if (print) {
+					  	WindowObject.document.close();
+					    WindowObject.focus();
+					    WindowObject.print();
+					    WindowObject.close();  		
+				  	}	
+
+			}); 		
+ 		
+	};
+
+	// prints hidden table //
+	// will be used to print image //
+	$scope.displayHiddenDiagram = function() {
+		var $ = go.GraphObject.make;
+		$scope.diagram = $(go.Diagram, "print_diagram");
+		var createTableHeaders = function() {
+			 // create initial table
+			 var table = $(go.Panel, "Table",{defaultRowSeparatorStroke: "gray",defaultColumnSeparatorStroke: "gray"})
+			 var row = $(go.Panel, "TableRow", {background: '#00008B',row:0});
+			  row.add($(go.TextBlock, "Name",{stroke:"white", width:100,column: 0, font: "bold 10pt sans-serif", margin: 6,textAlign:"left"}));
+			  row.add($(go.TextBlock, "Still Living",{stroke:"white", width:100,column: 1, font: "bold 10pt sans-serif", margin: 6,textAlign:"left"}));
+			 for (key in $scope.disease_list) {
+			   row.add($(go.TextBlock, $scope.disease_list[key].translatedDiseaseName,{stroke:"white", width:90,column: parseInt(key)+2, font: "bold 10pt sans-serif", margin: 6,textAlign:"left"}));
+			 }
+			 table.add(row);
+			 
+			 for (key in $scope.pi) {
+			   row = $(go.Panel, "TableRow", {row:parseInt(key)+3});
+			     row.add($(go.TextBlock, $scope.pi[key].name+"\n("+$scope.pi[key].relationship+")",{ width:100,column: 0, font: "bold 10pt sans-serif", margin: 6,textAlign:"left"}));
+			     row.add($(go.TextBlock, $scope.pi[key].is_alive,{ width:100,column: 1, font: "bold 10pt sans-serif", margin: 6,textAlign:"left"}));
+			   
+			   for (col in $scope.disease_list) {
+			     row.add($(go.TextBlock, $scope.lookupDisease($scope.pi[key],$scope.disease_list[col].code,true),{ width:90,column: parseInt(col)+2, font: "bold 10pt sans-serif", margin: 6,textAlign:"left"}));
+			   } 
+			   table.add(row)
+			 }
+			 
+			 return table
+		}
+		
+		g = $(go.Part, "Auto",
+		       $(go.Shape, { fill: "white", stroke: "gray", strokeWidth: 3 }),
+		         createTableHeaders()
+		    )
+		 $scope.diagram.add(g);
+	};
+
+	// closes the modal window //
+	$scope.close = function () {
 		$modalInstance.close();
-		console.log($scope.personal_information);
 		$("#optionsPanelMain").dialog('destroy').remove()
 	};
 }]);
