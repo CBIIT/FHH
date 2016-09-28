8d4ac1a5e6e1b3376e800a6c2c0055da8270cc82
Subhash Vallala <sgvallala@gmail.com>
2016-09-28 13:13:03 -0400

    MFHP-677 changes

---
+++ js/keys.js
@@ -0,0 +1,13 @@
+
+// Dropbox Keys
+var DROPBOX_APP_KEY = '5qz5g5nkzdzwpd3';
+
+// Google Drive Keys
+var GOOGLE_CLIENT_ID = '459770573635-ohh6qdb4gu4i8nlnlap609oogsoa0ub8.apps.googleusercontent.com';
+var GOOGLE_SCOPES = 'https://www.googleapis.com/auth/drive.file';
+
+// Health Vault Settings
+var HEATH_VAULT_APP_KEY = '4f0cc940-7bdb-436e-abc6-9f000f51cf1e';
+var HEATH_VAULT_PROXY_SERVER = 'https://account.healthvault-ppe.com'
+var FHH_PROXY_PORT = "";
+var FHH_SITE_PORT = "";
