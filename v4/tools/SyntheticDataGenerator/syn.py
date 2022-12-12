d612dcc86a23fccfc59c5b2c5b1a2cb97b434f45
Black <blacklm@nih.gov>
2022-12-12 11:31:41 -0500

    Updated birthdate to be realistic

--- v4/tools/SyntheticDataGenerator/syn.py
+++ v4/tools/SyntheticDataGenerator/syn.py
@@ -11,8 +11,8 @@ data={}
 
 def create_dod(inDate):
     # Using builtin support of timedelta, add 18 years (in days accounting for up to 4 leap years), and 100 years as a cap
-    age_min_offset = 6574
-    age_max_offset = 36900
+    age_min_offset = 9500       #Minimum age at death is around 26
+    age_max_offset = 36900      #Maximum age at death is aroung 100
     dod_min  = inDate + timedelta(days=age_min_offset)
     dod_max = inDate + timedelta(days=age_max_offset)
     if(dod_max - date.today() > timedelta(0)):
@@ -21,13 +21,9 @@ def create_dod(inDate):
     return dod_out
 
 
-def datagenerate_person(person_id, sex, father = None, mother = None, p_gpa = None, p_gma = None, m_gpa = None, m_gma = None):
+def datagenerate_person(person_id, sex, dob, father = None, mother = None, p_gpa = None, p_gma = None, m_gpa = None, m_gma = None, ):
     
-    #sex = np.random.choice(["Male", "Female"], p=[0.5, 0.5]) 
-        #how to put the random sex in the siblings and potentially the generations below proband
     adopted = np.random.choice(["In", "Out", "No"], p=[0.0023, 0.0002, 0.9975])
-    dob = fake.date_of_birth(minimum_age=18, maximum_age=100)
-    dob_string = dob.strftime('%m/%d/%Y')
     deceased = bool(np.random.choice([True, False], p=[0.25, 0.75]))
     #multiBirthtype = np.random.choice(["Fraternal", "Identical", "Triplets", " "], p=[0.03, 0.01, 0.005, 0.955])
     race = np.random.choice(["American Indian or Alaskan Native", "Asian", "Native Hawaiian or Other Pacific Islander", "Black or African American", "White", "Other"], 
@@ -40,7 +36,7 @@ def datagenerate_person(person_id, sex, father = None, mother = None, p_gpa = No
             "Patient ID": person_id,
             "demographics": {
                 "gender": sex,
-                "birthdate":dob_string,
+                "birthdate":dob.strftime('%m/%d/%Y'),
                 "deathdate":create_dod(dob).strftime('%m/%d/%Y') if deceased==True else "",
             },
 
@@ -76,12 +72,13 @@ def datagenerate_person(person_id, sex, father = None, mother = None, p_gpa = No
 
 ###############################################
 
-def make_children(num_children, father_id, mother_id):
+def make_children(num_children, dob, father_id, mother_id):
     children = []
     for i in range(num_children):
         person_id = fake.uuid4()
         sex_assigned = np.random.choice(["Male", "Female"], p=[0.5, 0.5]) 
-        person = datagenerate_person(person_id, sex_assigned, father_id, mother_id)
+        #fathers_surname
+        person = datagenerate_person(person_id, sex_assigned, dob,  father_id, mother_id)
         #print(person_id)
         children.append(person_id)
         data["people"][person_id] = person
@@ -103,23 +100,32 @@ if __name__ == '__main__':
     
     m_gpa_id = fake.uuid4()
     m_gma_id = fake.uuid4()
-    
-    probandsex = np.random.choice(["Male", "Female"], p=[0.5, 0.5])
-########################################################
-
-    proband = datagenerate_person(proband_id, probandsex, father_id, mother_id)
-
-    father = datagenerate_person(father_id, "Male", p_gpa_id, p_gma_id)
-    mother = datagenerate_person(mother_id, "Female", m_gpa_id, m_gma_id)
-    
-    p_gpa = datagenerate_person(p_gpa_id, "Male")
-    p_gma = datagenerate_person(p_gma_id, "Female")
-
-    m_gpa = datagenerate_person(m_gpa_id, "Male")
-    m_gma = datagenerate_person(m_gma_id, "Female")
 
 #######################################################
 
+    g_grandparent_gen_dob = fake.date_of_birth(minimum_age=109, maximum_age=111)
+    grandparent_gen_dob = fake.date_of_birth(minimum_age=90, maximum_age=94)
+    parent_gen_dob = fake.date_of_birth(minimum_age=72, maximum_age=76)
+    proband_gen_dob = fake.date_of_birth(minimum_age=54, maximum_age=58)
+    kids_gen_dob = fake.date_of_birth(minimum_age=36, maximum_age=40)
+    grandkids_gen_dob = fake.date_of_birth(minimum_age=18, maximum_age=22)
+    g_grandkids_gen_dob = fake.date_of_birth(minimum_age=0, maximum_age=1)
+
+########################################################    
+
+    probandsex = np.random.choice(["Male", "Female"], p=[0.5, 0.5])
+    proband = datagenerate_person(proband_id, probandsex, proband_gen_dob, father_id, mother_id)
+
+    father = datagenerate_person(father_id, "Male", parent_gen_dob, p_gpa_id, p_gma_id)
+    mother = datagenerate_person(mother_id, "Female", parent_gen_dob, m_gpa_id, m_gma_id)
+    
+    p_gpa = datagenerate_person(p_gpa_id, "Male", grandparent_gen_dob)
+    p_gma = datagenerate_person(p_gma_id, "Female", grandparent_gen_dob)
+
+    m_gpa = datagenerate_person(m_gpa_id, "Male", grandparent_gen_dob)
+    m_gma = datagenerate_person(m_gma_id, "Female", grandparent_gen_dob)
+
+#######################################################
     data["proband"] = proband_id
     data["people"] = {}
 
@@ -136,22 +142,22 @@ if __name__ == '__main__':
 
     num_children = 1
     #num_children = np.random.randint(1, 4)
-    siblings = make_children(num_children, father_id, mother_id)
-    fathers_siblings = make_children(num_children, p_gpa_id, p_gma_id)
-    mothers_siblings = make_children(num_children, m_gpa_id, m_gma_id)
-    proband_children = make_children(num_children, proband_id, None)
+    siblings = make_children(num_children, proband_gen_dob, father_id, mother_id)
+    fathers_siblings = make_children(num_children, parent_gen_dob, p_gpa_id, p_gma_id)
+    mothers_siblings = make_children(num_children, parent_gen_dob, m_gpa_id, m_gma_id)
+    proband_children = make_children(num_children, kids_gen_dob, proband_id, None)
 
     for person_id in siblings:
-        siblings_children = make_children(num_children, person_id, None)
+        siblings_children = make_children(num_children, kids_gen_dob, person_id, None)
 
     for person_id in fathers_siblings:
-        fathers_siblings_children = make_children(num_children, person_id, None)
+        fathers_siblings_children = make_children(num_children, proband_gen_dob, person_id, None)
 
     for person_id in mothers_siblings:
-        mothers_siblings_children = make_children(num_children, person_id, None)
+        mothers_siblings_children = make_children(num_children, proband_gen_dob, person_id, None)
 
     for person_id in proband_children:
-        proband_grandchildren = make_children(num_children, person_id, None)
+        proband_grandchildren = make_children(num_children, grandkids_gen_dob, person_id, None)
 
 
     print(json.dumps(data, indent=4))
