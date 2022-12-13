ec1e6aad0d79ab64ec24c452f3d9708c232599ba
Black <blacklm@nih.gov>
2022-12-13 12:00:04 -0500

    Updated Birthdate V2 and Diseases DataFrame Started

--- v4/tools/SyntheticDataGenerator/syn.py
+++ v4/tools/SyntheticDataGenerator/syn.py
@@ -2,6 +2,7 @@ import json
 from faker import Faker
 from datetime import timedelta, date
 import numpy as np
+import pandas as pd
 
 fake = Faker()
 
@@ -20,6 +21,19 @@ def create_dod(inDate):
     dod_out = fake.date_between_dates(date_start=dod_min, date_end=dod_max)
     return dod_out
 
+ppbNonCancer = {
+            'code':  ['E039', 'E069', 'J984', 'D1430', 'J939', 'N281', 'K635', 'E042', 'E063', 'E059', 'E041', 'D319', 'D332', 'D352', 'J90X', 'J339', 'E079', 'D34X', 'Q753', 'H3500'],
+            'name': ['Hypothryoidism, unspec.' , 'Thyroiditis, unspec.' ,'Lung Cyst','Benign lung growth' ,'Pneumothorax' ,'Renal/kidney cyst ','Intestinal polyps','Goiter',
+                    'Hashimoto thyroiditis','Hyperthyroidism','Thyroid cyst/thyroid nodule','Eye tumor','Benign brain tumor','Pituitary gland tumor','Pleural effusion','Nasal polyps/cyst',
+                    'Disorder of thyroid, unspecified','Thyroid Adenoma','Macrocephaly','Retinopathy'],
+            'shorthand': ['HypoThyr','Thyroiditis','Cyst, Lung','Bronchus/lung','Pneumothor','Cyst, Kidney','Polyps, Colon','Goiter','Thyroiditis/Hash','HyperThyr','Cyst, Thyroid','Tumor, Eye',
+                        'Tumor, Brain','Tumor, Pituit','Pleur eff','Cyst, Nasal','Thyr Dis','Thyroid adenoma','Macrocephaly','Retinopathy']
+        }
+
+df = pd.DataFrame(ppbNonCancer, columns = ['code','name','shorthand'])
+dd = df.sample()
+dj = dd.to_json()
+
 
 def datagenerate_person(person_id, sex, dob, father = None, mother = None, p_gpa = None, p_gma = None, m_gpa = None, m_gma = None, ):
     
@@ -30,6 +44,8 @@ def datagenerate_person(person_id, sex, dob, father = None, mother = None, p_gpa
         p=[0.013, 0.061, 0.003, 0.136, 0.758, 0.029])
     first_name = fake.first_name_male() if sex=="Male" else fake.first_name_female() 
     last_name = fake.last_name()
+    noncancer = np.random.choice(["Yes", "No"], p=[0.75, 0.25])
+
 
     proband = {
             "name": first_name + " " + last_name,
@@ -39,7 +55,9 @@ def datagenerate_person(person_id, sex, dob, father = None, mother = None, p_gpa
                 "birthdate":dob.strftime('%m/%d/%Y'),
                 "deathdate":create_dod(dob).strftime('%m/%d/%Y') if deceased==True else "",
             },
-
+            "diseases":{
+                "disease": dj if noncancer=="Yes" else '',
+            },
             "adopted": adopted,
             "deceased": deceased,
             "Race": race,
@@ -78,7 +96,7 @@ def make_children(num_children, dob, father_id, mother_id):
         person_id = fake.uuid4()
         sex_assigned = np.random.choice(["Male", "Female"], p=[0.5, 0.5]) 
         #fathers_surname
-        person = datagenerate_person(person_id, sex_assigned, dob,  father_id, mother_id)
+        person = datagenerate_person(person_id, sex_assigned, dob, father_id, mother_id)
         #print(person_id)
         children.append(person_id)
         data["people"][person_id] = person
@@ -102,6 +120,8 @@ if __name__ == '__main__':
     m_gma_id = fake.uuid4()
 
 #######################################################
+    def dob_generator(minimum_in, maximum_in):
+        return fake.date_of_birth(minimum_age=minimum_in, maximum_age=maximum_in)
 
     g_grandparent_gen_dob = fake.date_of_birth(minimum_age=109, maximum_age=111)
     grandparent_gen_dob = fake.date_of_birth(minimum_age=90, maximum_age=94)
@@ -114,16 +134,16 @@ if __name__ == '__main__':
 ########################################################    
 
     probandsex = np.random.choice(["Male", "Female"], p=[0.5, 0.5])
-    proband = datagenerate_person(proband_id, probandsex, proband_gen_dob, father_id, mother_id)
+    proband = datagenerate_person(proband_id, probandsex, dob_generator(54, 58), father_id, mother_id)
 
-    father = datagenerate_person(father_id, "Male", parent_gen_dob, p_gpa_id, p_gma_id)
-    mother = datagenerate_person(mother_id, "Female", parent_gen_dob, m_gpa_id, m_gma_id)
+    father = datagenerate_person(father_id, "Male", dob_generator(72, 76), p_gpa_id, p_gma_id)
+    mother = datagenerate_person(mother_id, "Female", dob_generator(72, 76), m_gpa_id, m_gma_id)
     
-    p_gpa = datagenerate_person(p_gpa_id, "Male", grandparent_gen_dob)
-    p_gma = datagenerate_person(p_gma_id, "Female", grandparent_gen_dob)
+    p_gpa = datagenerate_person(p_gpa_id, "Male", dob_generator(90, 94))
+    p_gma = datagenerate_person(p_gma_id, "Female", dob_generator(90, 94))
 
-    m_gpa = datagenerate_person(m_gpa_id, "Male", grandparent_gen_dob)
-    m_gma = datagenerate_person(m_gma_id, "Female", grandparent_gen_dob)
+    m_gpa = datagenerate_person(m_gpa_id, "Male", dob_generator(90, 94))
+    m_gma = datagenerate_person(m_gma_id, "Female", dob_generator(90, 94))
 
 #######################################################
     data["proband"] = proband_id
@@ -142,27 +162,22 @@ if __name__ == '__main__':
 
     num_children = 1
     #num_children = np.random.randint(1, 4)
-    siblings = make_children(num_children, proband_gen_dob, father_id, mother_id)
-    fathers_siblings = make_children(num_children, parent_gen_dob, p_gpa_id, p_gma_id)
-    mothers_siblings = make_children(num_children, parent_gen_dob, m_gpa_id, m_gma_id)
-    proband_children = make_children(num_children, kids_gen_dob, proband_id, None)
+    siblings = make_children(num_children, dob_generator(54, 58), father_id, mother_id)
+    fathers_siblings = make_children(num_children, dob_generator(72, 76), p_gpa_id, p_gma_id)
+    mothers_siblings = make_children(num_children, dob_generator(72, 76), m_gpa_id, m_gma_id)
+    proband_children = make_children(num_children, dob_generator(36, 40), proband_id, None)
 
     for person_id in siblings:
-        siblings_children = make_children(num_children, kids_gen_dob, person_id, None)
+        siblings_children = make_children(num_children, dob_generator(36, 40), person_id, None)
 
     for person_id in fathers_siblings:
-        fathers_siblings_children = make_children(num_children, proband_gen_dob, person_id, None)
+        fathers_siblings_children = make_children(num_children, dob_generator(54, 58), person_id, None)
 
     for person_id in mothers_siblings:
-        mothers_siblings_children = make_children(num_children, proband_gen_dob, person_id, None)
+        mothers_siblings_children = make_children(num_children, dob_generator(54, 58), person_id, None)
 
     for person_id in proband_children:
-        proband_grandchildren = make_children(num_children, grandkids_gen_dob, person_id, None)
+        proband_grandchildren = make_children(num_children, dob_generator(18, 22), person_id, None)
 
 
     print(json.dumps(data, indent=4))
-    
-    #print(siblings)
-    #print(fathers_siblings)
-    #print(mothers_siblings)
-    #print(grandchildren)
