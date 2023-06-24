b55836588df7cb6092789afd80153ca64d3e996f
bremonline <breml@nih.gov>
2023-06-24 12:56:10 -0400

    Updated the v3-v4 transforms

--- v4/tools/transforms/v3-v4.js
+++ v4/tools/transforms/v3-v4.js
@@ -1,4 +1,4 @@
-var v4 = [];
+var v4 = {};
 
 $(document).ready(function() {
 
@@ -14,16 +14,33 @@ $("#import_file").change(function(e) {
     xmlDoc = $.parseXML( e.target.result ),
     $xml = $( xmlDoc );
     $patient = $xml.find("patient patientPerson");
+
+    // Find the proband id and add it to the v4 object
+    var id = $patient.find("id").attr("extension");
+    v4.proband = id;
+
+    // Add the empty people list to the v4 object
+    v4.people = {};
+    // Add the proband as the first person in the list
+    v4.people[id] = {"name":"Test"};
+
     $patient.find("relative relationshipHolder").each(function() {
 
       id = $(this).find("id").attr("extension");
       name = $(this).find("name").attr("formatted");
 
-      if (id && name && name != 'undefined') console.log(id + ": " + name);
+      if (id && name && name != 'undefined') {
+        v4.people[id] = {"name":name};
+      }
 
     });
   };
   document.getElementById('import_file').value= null; // resets the value to allow reload
+  console.log(v4);
 });
 
 });
+
+function add_proband_to_v4() {
+  v4.proband = "test"
+}
